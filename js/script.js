dc7ba736a2e3f059eb50c54d8609b9c0e96704aa
S2nexhello <ranvijays3nex@gmail.com>
2024-01-17 21:28:30 +0530

    First Commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+
+
+// <!-- typed js effect starts -->
+var typed = new Typed(".typing-text", {
+    strings: ["Data Analyst", "Business Analyst", "PowerBi Developer", "SQL Developer", "Web Developer"],
+    loop: true,
+    typeSpeed: 95,
+    backSpeed: 25,
+    backDelay: 500,
+});
+// <!-- typed js effect ends -->
+
